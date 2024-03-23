59e6275a1f8c4a8e1b56bd77920a4b4fe0984fe2
MihailPaduraru <133820408+MihailPaduraru@users.noreply.github.com>
2024-03-23 22:38:44 +0000

    Deploying to gh-pages from @ MihailPaduraru/goit-react-hw-08-phonebook@326dbc96299eb86f693325c375e58c8c7e1fbe29 🚀

---
+++ static/js/772.3852d592.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[772],{6772:(e,n,a)=>{a.r(n),a.d(n,{default:()=>g});var r,t=a(7061),s=a(7528);const c=a(4512).Ay.main(r||(r=(0,s.A)(["\n  min-height: 100vh;\n  background: linear-gradient(\n    172deg,\n    rgba(122, 182, 191, 0.24135591736694673) 0%,\n    rgba(20, 67, 152, 0.03687412464985995) 35%,\n    rgba(243, 132, 140, 0.28617384453781514) 100%\n  );\n"])));var h=a(579);const g=()=>(0,h.jsx)(c,{children:(0,h.jsx)(t.s4,{})})}}]);
+//# sourceMappingURL=772.3852d592.chunk.js.map
